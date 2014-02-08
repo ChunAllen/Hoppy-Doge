4314ccdcc03529f73697e0e98164afc7ffcc06b8
Robbie Marcelo <rbmrclo@hotmail.com>
2014-02-09 00:20:36 +0800

    Removes dummy character and WIP canvas drawing

---
+++ public/javascripts/canvas.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  var canvas = document.getElementById('canvasId');
+  var canvas2d = canvas.getContext('2d');
+
+  function drawRect() {
+    canvas2d.fillStyle = "rgb(200,0,0)";
+    canvas2d.fillRect (10, 10, 55, 50);
+
+    canvas2d.fillStyle = "rgba(0, 0, 200, 0.5)";
+    canvas2d.fillRect (30, 30, 55, 50);
+  }
+
+  drawRect();
+});
