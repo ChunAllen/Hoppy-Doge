7de2b9ce14440e84bc017f0c3fbc9e9ad8309299
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-02-08 15:14:30 +0800

    Adds Blinks

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -12,6 +12,19 @@ $(function(){
 
     var context = document.getElementById('canvasId').getContext("2d");
 
+    var breathInc = 0.1,
+        breathDir = 1,
+        breathAmt = 0,
+        breathMax = 2,
+        breathInterval = setInterval(updateBreath, 1000 / fps);
+
+    var maxEyeHeight = 14,
+        curEyeHeight = maxEyeHeight,
+        eyeOpenTime = 0,
+        timeBtwBlinks = 4000,
+        blinkUpdateTime = 200,
+        blinkTimer = setInterval(updateBlink, blinkUpdateTime);
+
     _.each(imageArray, function(name){
         loadImage(name);
     });
@@ -37,19 +50,20 @@ $(function(){
             y = charY;
 
         context.width = context.width;
-        context.drawImage(images["leftArm"], x + 40, y - 42);
+        context.drawImage(images["leftArm"], x + 40, y - 42 - breathAmt);
         context.drawImage(images["legs"], x, y);
         context.drawImage(images["torso"], x, y - 50);
-        context.drawImage(images["rightArm"], x - 15, y - 42);
-        context.drawImage(images["head"], x - 10, y - 125);
-        context.drawImage(images["hair"], x - 37, y - 138);
+        context.drawImage(images["rightArm"], x - 15, y - 42 - breathAmt);
+        context.drawImage(images["head"], x - 10, y - 125 - breathAmt);
+        context.drawImage(images["hair"], x - 37, y - 138 - breathAmt);
 
-        drawEllipse(x + 47, y - 68, 8, 14); // Left Eye
-        drawEllipse(x + 58, y - 68, 8, 14); // Right Eye
+        drawEllipse(x + 47, y - 68 - breathAmt, 8, curEyeHeight); // Left Eye
+        drawEllipse(x + 58, y - 68 - breathAmt, 8, curEyeHeight); // Right Eye
 
-        drawEllipse(x + 40, y + 29, 160, 6); // Shadow
+        drawEllipse(x + 40, y + 29, 160 - breathAmt, 6); // Shadow
     }
 
+    //Game Utils
     function drawEllipse(centerX, centerY, width, height) {
 
       context.beginPath();
@@ -69,6 +83,39 @@ $(function(){
       context.fillStyle = "black";
       context.fill();
       context.closePath();
+    }
+
+    function updateBreath() {
+        if (breathDir === 1) {
+            breathAmt -= breathInc;
+            if (breathAmt < - breathMax) {
+                breathDir = -1;
+            }
+        } else {
+            breathAmt += breathInc;
+            if (breathAmt > breathMax) {
+                breathDir = 1;
+            }
+        }
+    }
+
+    //Blinking eyes
+    function updateBlink() {
+        eyeOpenTime += blinkUpdateTime;
+
+        if(eyeOpenTime >= timeBtwBlinks) {
+            blink();
+        }
+    }
+
+    function blink() {
+        curEyeHeight -= 1;
+        if (curEyeHeight <= 0) {
+            eyeOpenTime = 0;
+            curEyeHeight = maxEyeHeight;
+        } else {
+            setTimeout(blink, 10);
+        }
     }
 
 });
