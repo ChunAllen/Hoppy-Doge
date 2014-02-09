4de601d0e5267a6cbad0e7951d0d69706083ebd3
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-02-09 17:03:47 +0800

    Adds Leader

--- public/javascripts/leaderboard.js
+++ public/javascripts/leaderboard.js
@@ -1,22 +1,35 @@
 $(function(){
-    rootRef = new Firebase('https://hoppydoge.firebaseio.com/leaderboard');
+    var rootRef = new Firebase('https://hoppydoge.firebaseio.com/leaderboard');
 
     window.Leaderboard = {}
 
     Leaderboard.pushName = function(name, score) {
         newRef = rootRef.push();
+        if (name =="") { name = "YourName" }
         newRef.set({name: name, score: score});
-        if (score == 0) score = 1;
+        if (score == 0) {score = 1};
         newRef.setPriority(1/score);
 
         return true;
     }
 
     Leaderboard.showAll = function(callback) {
-        var limit = 10,
-            query = "https://hoppydoge.firebaseio.com/leaderboard.json?limit=" + limit;
-        $.get(query, function(data){
-            callback(data);
+        var limit = 10;
+        var query = rootRef.startAt().limit(limit);
+        query.on('value', function(childSnapshot){
+            callback(childSnapshot.val());
         });
+
     }
+
+    Leaderboard.showAll(function(leaders){
+        $("#scoreboard tbody").html();
+        _.each(leaders, function(leader){
+            $("#scoreboard tbody").append("<tr>" +
+                    "<td>"+ leader.name +"</td>" +
+                    "<td>"+ leader.score +"</td>" +
+                "</tr>"
+            )
+        });
+    });
 });
